36e33cf25319c00e9bdb0e862302e927afb6e7da
shaniya <shaniya@uwo.ca>
2024-11-30 10:45:22 -0500

    Unauth mission created but needs to be filled with real text because it has mock text in it rn

--- client/src/views/UnauthView.jsx
+++ client/src/views/UnauthView.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import UnauthNavbar from '../components/UnauthNavbar'
 import UnauthHero from '../components/UnauthHero'
+import UnauthOurMission from '../components/UnauthOurMission'
 
 const UnauthView = () => {
   return (
     <div>
           <UnauthNavbar />
           <UnauthHero />
+          <UnauthOurMission />
     </div>
   )
 }
