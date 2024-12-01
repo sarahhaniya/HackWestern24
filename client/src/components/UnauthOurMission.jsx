c1f93c25a5ff95701d90c8d95c1c93f4e892dda8
shaniya <shaniya@uwo.ca>
2024-12-01 04:00:54 -0500

    mission updated

--- client/src/components/UnauthOurMission.jsx
+++ client/src/components/UnauthOurMission.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import '../styles/UnauthOurMission.css'
 
-const UnauthOurMission = () => {
+const UnauthOurMission = () =>
+{
   return (
     <div className='mission-container'>
-          <h2>Our Mission</h2>
-          <h3>Mental Health</h3>
-          <p className='mission-p'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </p>
-          <h3>Environment</h3>
-          <p className='mission-p'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </p>
+      <h2>Our Mission</h2>
+      <p className='mission-p'>At PickPark, our mission is to revolutionize the way employees commute by delivering a stress-free, efficient, and sustainable parking experience.
+        <br></br><br></br>
+        We understand the toll that parking stress takes on mental health. From circling crowded lots to worrying about finding a spot that meets specific needs, the daily parking struggle leaves employees frazzled and less productive before their day even begins. That’s why we’re committed to providing a solution that guarantees peace of mind. With PickPark, employees can reserve their ideal parking spot in advance, arriving confident and ready to take on their day.
+        <br></br><br></br>
+        Beyond improving individual well-being, PickPark is designed to make a positive impact on the environment. By reducing unnecessary idling and promoting green commuting options like carpooling and EV charging, our platform supports sustainability goals and helps employers reduce their carbon footprint.
+        <br></br><br></br>
+        With dynamic parking management tailored to hybrid work schedules, PickPark is more than a parking system—it’s a step toward happier employees, healthier workplaces, and a greener future.
+        <br></br><br></br>
+        Join us in reshaping the commute, one parking spot at a time. </p>
+
     </div>
   )
 }
