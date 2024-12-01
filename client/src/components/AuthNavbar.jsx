0da67725db30e96caa4f7c4640cb8b2ba3049387
shaniya <shaniya@uwo.ca>
2024-11-30 20:42:51 -0500

    AuthNavbar created for the authview

---
+++ client/src/components/AuthNavbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import navbarLogo from '../assets/lightLogo.png'
+const AuthNavbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo-container">
+                <a href="/" className="navbar-logo-link">
+                    <img src={navbarLogo} alt="PickPark Logo" className="navbar-logo" />
+                </a>
+            </div>
+            <div className="nav-links">
+                <a href="/" className="nav-link">SIGN OUT</a>
+            </div>
+        </nav>);
+}
+
+export default AuthNavbar
