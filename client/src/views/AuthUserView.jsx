253d7ad83e6e678860d89484be81db51a8ab70b9
OmeetKS <152014113+OmeetKS@users.noreply.github.com>
2024-12-01 07:01:59 -0500

    Update AuthUserView.jsx

--- client/src/views/AuthUserView.jsx
+++ client/src/views/AuthUserView.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import AuthNavbar from '../components/AuthNavbar';
+
 
 const AuthUserView = () => {
   return (
     <div>
+      <AuthNavbar />
       <h1>Welcome, User!</h1>
       <p>This is the dashboard for regular users.</p>
     </div>
